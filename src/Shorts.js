77676b107ff13322ea29845d3b5ad219188dcf48
Sanghean Lee <francesco419@naver.com>
2022-06-09 21:40:44 +0900

    4.5

--- src/Shorts.js
+++ src/Shorts.js
@@ -3,6 +3,7 @@ import Menu from "./Menu";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Loading from "./Loading";
+import { ReactComponent as Good } from './img/good.svg';
 
 const API_KEY=process.env.REACT_APP_YOUTUBE_API_KEY;
 
@@ -25,9 +26,38 @@ function Shorts(){
         console.log(videos)
     },[])
 
-    function ShortVideos(){
+    function ShortVideos({thumbnails,title,channelTitle}){
         return(
-            <div></div>
+            <div className={styles.short}>
+                <div className={styles.shortvideo}>
+                    <div className={styles.video}>
+                        <img src={thumbnails}/>
+                        <div className={styles.title}>
+                            <p>{title}</p>
+                            <div className={styles.name}>
+                                <div className={styles.channelname}>
+                                    <img src={thumbnails}/>
+                                    <p>{channelTitle}</p>
+                                </div>
+                                <div className={styles.subscribe}>구독</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.menu}>
+                        <div className={styles.good}>
+                            <Good style={{fill:'#7e7e7e'}}/>
+                            <p>좋아요</p>
+                            <div className={styles.like}> 이 동영상이 마음에 듭니다.</div>
+                        </div>
+                        <div className={styles.bad}>
+                            <Good style={{fill:'#7e7e7e',transform: 'rotate(180deg)'}}/>
+                            <p>싫어요</p>
+                        </div>
+                        <p>댓글</p>
+                        <p>공유</p>
+                    </div>
+                </div>
+            </div>
         )
     }
 
@@ -35,28 +65,17 @@ function Shorts(){
         <div>
             <Menu/>
             <div className={styles.box}>
-                {loading ? <Loading/> :(
-                    <div className={styles.short}>
-                        <div className={styles.shortvideo}>
-                            <div className={styles.video}>
-                                <img src={videos.items[0].snippet.thumbnails.high.url}/>
-                                <div className={styles.title}>
-                                    <p>{videos.items[0].snippet.title}</p>
-                                    <div className={styles.name}>
-                                        <div className={styles.channelname}>
-                                            <img src={videos.items[0].snippet.thumbnails.high.url}/>
-                                            <p>{videos.items[0].snippet.channelTitle}</p>
-                                        </div>
-                                        <div className={styles.subscribe}>구독</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className={styles.menu}>
-                                B
-                            </div>
-                        </div>
-                    </div>
-                )}
+                {loading ? <Loading/> :
+                <div>
+                    {videos.items.map((item)=>(
+                        <ShortVideos
+                    thumbnails={item.snippet.thumbnails.high.url}
+                    title={item.snippet.title}
+                    channelTitle={item.snippet.channelTitle}
+                    />
+                    ))}
+                </div>
+                }
             </div>
         </div>
     )
